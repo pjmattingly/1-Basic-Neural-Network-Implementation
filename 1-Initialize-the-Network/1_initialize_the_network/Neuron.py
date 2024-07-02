4803c6e1d0ef580792e318ac621b2e9ca1c9c3f4
Peter Mattingly <pjmattingly@users.noreply.github.com>
2024-07-02 01:34:50 -0600

    adding exception on bad input

--- 1-Initialize-the-Network/1_initialize_the_network/Neuron.py
+++ 1-Initialize-the-Network/1_initialize_the_network/Neuron.py
@@ -1,6 +1,9 @@
 import random
 
 class Neuron:
-    def __init__(self, inputs):
+    def __init__(self, inputs : int):
+        if (type(inputs) is not int) or (inputs < 0):
+            raise TypeError("Argument 'inputs' should be positive integer.")
+        
         self._weights = [random.random() for x in range(inputs)]
         self._bias = random.random()
