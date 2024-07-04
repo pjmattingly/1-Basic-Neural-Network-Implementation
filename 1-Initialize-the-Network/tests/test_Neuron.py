01c3a885329132307d620df84de8be988e527105
Peter Mattingly <pjmattingly@users.noreply.github.com>
2024-07-03 19:47:43 -0600

    adding test ficture to instantiate Neuron

--- 1-Initialize-the-Network/tests/test_Neuron.py
+++ 1-Initialize-the-Network/tests/test_Neuron.py
@@ -1,6 +1,10 @@
 import pytest
 from init_the_network.Neuron import Neuron
 
+@pytest.fixture
+def inst():
+    return Neuron(3)
+
 class Test__init__:
     def test_bad_input_not_int(self):
         with pytest.raises(TypeError):
@@ -11,6 +15,7 @@ class Test__init__:
             Neuron(-1)
 
 class Test_relu:
-    def test_bad_input(self):
+    def test_bad_input(self, inst):
         with pytest.raises(TypeError):
-            Neuron._relu("some bad input")
+            #Neuron._relu("some bad input")
+            inst._relu("some bad input")
