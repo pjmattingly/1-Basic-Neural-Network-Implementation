5d3defd544b9eab4ea562f4536b2e9d8698c67e5
Peter Mattingly <pjmattingly@users.noreply.github.com>
2024-07-03 18:57:00 -0600

    revising tests given code review on Neuron

--- 1-Initialize-the-Network/tests/test_Neuron.py
+++ 1-Initialize-the-Network/tests/test_Neuron.py
@@ -3,9 +3,9 @@ from init_the_network.Neuron import Neuron
 
 class Test__init__:
     def test_bad_input_not_int(self):
-        with pytest.raises(TypeError, match=r".*positive integer.*"):
+        with pytest.raises(TypeError):
             Neuron("some bad input")
 
     def test_bad_input_not_positive_int(self):
-        with pytest.raises(TypeError, match=r".*positive integer.*"):
+        with pytest.raises(ValueError):
             Neuron(-1)
