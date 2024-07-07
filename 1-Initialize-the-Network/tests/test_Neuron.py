96a86e980e63914130c3b7856c88d67697877b79
Peter Mattingly <pjmattingly@users.noreply.github.com>
2024-07-06 21:19:03 -0600

    testing forward_pass

--- 1-Initialize-the-Network/tests/test_Neuron.py
+++ 1-Initialize-the-Network/tests/test_Neuron.py
@@ -1,5 +1,6 @@
 import pytest
 from init_the_network.Neuron import Neuron
+import numpy as np
 
 @pytest.fixture
 def inst():
@@ -18,3 +19,38 @@ class Test_relu:
     def test_bad_input(self, inst):
         with pytest.raises(TypeError):
             inst._relu("some bad input")
+
+class Test_forward_pass:
+    def test_string_input(self, inst):
+        with pytest.raises(TypeError):
+            inst.forward_pass("some bad input")
+
+    def test_noniterable_input(self, inst):
+        with pytest.raises(TypeError):
+            inst.forward_pass(0)
+
+    def test_nonnumeric_input_1(self, inst):
+        with pytest.raises(ValueError):
+            inst.forward_pass(["a", "b", "c"])
+
+    def test_nonnumeric_input_2(self, inst):
+        with pytest.raises(ValueError):
+            inst.forward_pass([1, 2, "c"])
+
+    def test_special_nonnumeric_input_1(self, inst):
+        with pytest.raises(ValueError):
+            inst.forward_pass([1, 2, np.inf])
+
+    def test_special_nonnumeric_input_2(self, inst):
+        with pytest.raises(ValueError):
+            inst.forward_pass([1, 2, np.nan])
+
+    def test_special_nonnumeric_input_3(self, inst):
+        with pytest.raises(ValueError):
+            inst.forward_pass([1, 2, None])
+
+    def test_special_nonnumeric_input_4(self, inst):
+        with pytest.raises(ValueError):
+            inst.forward_pass([1, 2, np.newaxis])
+
+            #see: https://numpy.org/doc/stable/reference/constants.html#numpy.newaxis
