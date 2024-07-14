9642f5c54a286df132d04b957435107d54851c1d
Peter Mattingly <pjmattingly@users.noreply.github.com>
2024-07-13 18:55:56 -0600

    adding testing for forward_pass methods

--- 1-Initialize-the-Network/tests/test_Layer.py
+++ 1-Initialize-the-Network/tests/test_Layer.py
@@ -1,5 +1,6 @@
 import pytest
 from init_the_network.Layer import Layer
+import numpy as np
 
 @pytest.fixture
 def inst():
@@ -21,3 +22,42 @@ class Test__init__:
     def test_size_bad_input_not_positive(self):
         with pytest.raises(ValueError):
             Layer(3, -1)
+
+class Test_forward_pass:
+    def test_string_input(self, inst):
+        with pytest.raises(TypeError):
+            inst.forward_pass("str")
+
+    def test_noniterable_input(self, inst):
+        with pytest.raises(TypeError):
+            inst.forward_pass(0)
+
+    def test_nonnumeric_input_1(self, inst):
+        with pytest.raises(TypeError):
+            inst.forward_pass(["a", "b", "c"])
+
+    def test_nonnumeric_input_2(self, inst):
+        with pytest.raises(TypeError):
+            inst.forward_pass([1, 2, "c"])
+
+    def test_special_nonnumeric_input_1(self, inst):
+        with pytest.raises(ValueError):
+            inst.forward_pass([1, 2, np.inf])
+
+    def test_special_nonnumeric_input_2(self, inst):
+        with pytest.raises(ValueError):
+            inst.forward_pass([1, 2, np.nan])
+
+    def test_special_nonnumeric_input_3(self, inst):
+        with pytest.raises(TypeError):
+            inst.forward_pass([1, 2, None])
+
+    def test_special_nonnumeric_input_4(self, inst):
+        with pytest.raises(TypeError):
+            inst.forward_pass([1, 2, np.newaxis])
+
+            #see: https://numpy.org/doc/stable/reference/constants.html#numpy.newaxis
+
+    def test_wrong_size_inputs(self, inst):
+        with pytest.raises(ValueError):
+            inst.forward_pass([1, 2, 3, 4])
--- 1-Initialize-the-Network/tests/test_Network.py
+++ 1-Initialize-the-Network/tests/test_Network.py
@@ -1,5 +1,6 @@
 import pytest
 from init_the_network.Network import Network
+import numpy as np
 
 @pytest.fixture
 def inst():
@@ -21,3 +22,42 @@ class Test__init__:
     def test_inputs_bad_input_neg_int(self):
         with pytest.raises(ValueError):
             Network([-1])
+
+class Test_forward_pass:
+    def test_string_input(self, inst):
+        with pytest.raises(TypeError):
+            inst.forward_pass("str")
+
+    def test_noniterable_input(self, inst):
+        with pytest.raises(TypeError):
+            inst.forward_pass(0)
+
+    def test_nonnumeric_input_1(self, inst):
+        with pytest.raises(TypeError):
+            inst.forward_pass(["a", "b", "c"])
+
+    def test_nonnumeric_input_2(self, inst):
+        with pytest.raises(TypeError):
+            inst.forward_pass([1, 2, "c"])
+
+    def test_special_nonnumeric_input_1(self, inst):
+        with pytest.raises(ValueError):
+            inst.forward_pass([1, 2, np.inf])
+
+    def test_special_nonnumeric_input_2(self, inst):
+        with pytest.raises(ValueError):
+            inst.forward_pass([1, 2, np.nan])
+
+    def test_special_nonnumeric_input_3(self, inst):
+        with pytest.raises(TypeError):
+            inst.forward_pass([1, 2, None])
+
+    def test_special_nonnumeric_input_4(self, inst):
+        with pytest.raises(TypeError):
+            inst.forward_pass([1, 2, np.newaxis])
+
+            #see: https://numpy.org/doc/stable/reference/constants.html#numpy.newaxis
+
+    def test_wrong_size_inputs(self, inst):
+        with pytest.raises(ValueError):
+            inst.forward_pass([1, 2, 3, 4])
